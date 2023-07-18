ff3c221bd050aade312dc7209d324e5fb1bae450
daniel <dsbadal@gmail.com>
2023-07-18 09:12:15 -0700

    Generate alter-genome command text to match render

--- src/commonMain/kotlin/com/bedalton/creatures/breed/render/cli/RenderBreedCommand.kt
+++ src/commonMain/kotlin/com/bedalton/creatures/breed/render/cli/RenderBreedCommand.kt
@@ -277,6 +277,13 @@ class RenderBreedCommand(
         colorValues.contains('*') || colorValues.contains("rand")
     }
 
+    private val shouldPrintAlterGenomeCommand by option(
+        Flag,
+        fullName = "print-alter-genome-command",
+        description = "Print an equivalent alter genome statement minus the filename"
+    ).default(false)
+
+
     override fun execute() {
         GlobalScope.launch {
             executeSuspending()
@@ -582,6 +589,10 @@ class RenderBreedCommand(
 
         if (usesRandom) {
             logValuesForRandom(task)
+        }
+
+        if (shouldPrintAlterGenomeCommand) {
+            logAlterGenomeCommandText(task, gameVariant)
         }
         // Write PNG bytes to output file
         return if (wroteAll) 0 else 1
@@ -890,6 +901,95 @@ class RenderBreedCommand(
         return this
     }
 
+    private fun logAlterGenomeCommandText(task: BreedRendererBuilder, variant: GameVariant?) {
+        val out = StringBuilder(BOLD)
+            .append(BLACK_BACKGROUND)
+            .append(WHITE)
+            .append(UNDERLINE_WHITE)
+            .append("AlterGenomeCommand:\nrequires appending output file name\nMust ensure path to breed-util is valid\n")
+            .append(RESET)
+            .append("breed-util alter-genome ")
+
+        genomePath.nullIfEmpty()?.also {
+            out
+                .append(" \"")
+                .append(it)
+                .append('"')
+
+        }
+
+        val appendBreed = append@{ part: String, breed: BreedKey? ->
+            if (breed?.genus == null || breed.breed == null) {
+                return@append false
+            }
+            val genusInt = breed.genus!!
+            val breedChar = breed.breed!!
+
+            val genus = getGenusString(genusInt, variant)
+                ?.lowercase()
+                ?: return@append false
+            out
+                .append(' ')
+                .append("--")
+                .append(part)
+                .append(' ')
+                .append(genus)
+                .append(':')
+                .append(breedChar)
+        }
+
+        val headBreed = task.getHeadBreed()
+        val bodyBreed = task.getBodyBreed()
+        val legBreed = task.getLegBreed()
+        val armBreed = task.getArmBreed()
+        val tailBreed = task.getTailBreed()
+        val hairBreed = task.getHairBreed()
+
+        val allBreeds = listOfNotNull(
+            headBreed,
+            bodyBreed,
+            legBreed,
+            armBreed,
+            tailBreed,
+            hairBreed
+        ).distinct()
+
+        if (allBreeds.isNotEmpty()) {
+            if (allBreeds.size == 1) {
+                appendBreed("breed", allBreeds[0])
+            } else {
+                appendBreed("head", headBreed)
+                appendBreed("body", bodyBreed)
+                appendBreed("legs", legBreed)
+                appendBreed("arms", armBreed)
+                appendBreed("tail", tailBreed)
+                appendBreed("hair", hairBreed)
+            }
+        }
+
+
+        val appendColor = append@{color: String, value: Int? ->
+            if (value == null) {
+                return@append
+            }
+            out
+                .append(' ')
+                .append("--")
+                .append(color)
+                .append(' ')
+                .append(value)
+        }
+
+        task.getPaletteTransform()?.also { transform ->
+            appendColor("red", transform.red)
+            appendColor("green", transform.green)
+            appendColor("blue", transform.blue)
+            appendColor("swap", transform.swap)
+            appendColor("rotation", transform.rotation)
+        }
+
+        Log.i(out::toString)
+    }
 }
 
 
